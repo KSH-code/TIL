1ed3b1c7a754c91a1191211626ac17081eb3e695
KSH-code <tjdgnsqn133@gmail.com>
2018-02-26 18:02:12 +0900

    BOJ 백준온라인져지 1021 회전하는 큐 풀이

---
+++ algorithm/BOJ/1021/Main.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+/**
+ * https://www.acmicpc.net/contest/problem/1021
+ * BOJ 백준온라인져지 1021 회전하는 큐 풀이
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String str1[] = br.readLine().split(" ");
+        int N = Integer.parseInt(str1[0]);
+        int M = Integer.parseInt(str1[1]);
+        
+        Deque<Integer> deque = new LinkedList<>();
+        
+        for (int i = 1; i <= N; i++) {
+            deque.add(i);
+        }
+
+        String str2[] = br.readLine().split(" ");
+        int result = 0;
+        for (int z = 0; z < M; z++) {
+            int number = Integer.parseInt(str2[z]);
+            while (true) {
+                int idx = 0;
+                Iterator<Integer> it = deque.iterator();
+                while (it.hasNext()) {
+                    if (it.next() == number) break;
+                    idx++;
+                }
+                if (idx == 0) {
+                    deque.pollFirst();
+                    break;
+                } else if (deque.size() / 2 >= idx) {
+                    deque.addLast(deque.pollFirst());
+                    result++;
+                } else {
+                    deque.addFirst(deque.pollLast());
+                    result++;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
--- algorithm/BOJ/README.md
+++ algorithm/BOJ/README.md
@@ -7,6 +7,7 @@ int (*p)[N] = new int[M][N];
 * https://ps.kajebiii.ga/chest/boj/
 # 푼 문제
 ## 동아리
+* [1021](1021/Main.java)
 * [271/1](271/1/Main.java)
 * [10866](10866/Main.java)
 * [1966](1966/Main.cpp)
