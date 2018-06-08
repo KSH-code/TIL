6ad031c394c02de9ac923c94290ce566933649d0
ksh-code <tjdgnsqn133@gmail.com>
2018-06-08 17:54:20 +0900

    BOJ 백준온라인져지 11057 오르막 수 풀이

--- algorithm/BOJ/11057/Main.java
+++ algorithm/BOJ/11057/Main.java
@@ -14,7 +14,7 @@ public class Main {
         for (int i = 0; i < 10; i++) dp[1][i] = 1;
         for (int i = 2; i <= N; i++) {
             for (int j = 0; j < 10; j++) {
-                for (int k = 0; k <= j; k++) {
+                for (int k = 9; k >= j; k--) {
                     dp[i][j] += dp[i - 1][k];
                 }
                 dp[i][j] %= 10007;
