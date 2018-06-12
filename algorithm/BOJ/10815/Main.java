a47911f523a42eeae7264f4a86d3212b0cb4f3e1
ksh-code <tjdgnsqn133@gmail.com>
2018-06-12 17:15:44 +0900

    BOJ 백준온라인져지 10815 숫자 카드 풀이

--- algorithm/BOJ/10815/Main.java
+++ algorithm/BOJ/10815/Main.java
@@ -15,7 +15,7 @@ public class Main {
         for (int i = 0; i < N; i++) bitSet.set(Integer.parseInt(st.nextToken()) + 10000000);
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) bw.write((bitSet.get(Integer.parseInt(st.nextToken()) + 10000000) ? 1 : 0) + " ");
+        for (int i = 0; i < M; i++) bw.write((bitSet.get(Integer.parseInt(st.nextToken()) + 10000000) ? "1 " : "0 "));
         bw.flush();
     }
 }
