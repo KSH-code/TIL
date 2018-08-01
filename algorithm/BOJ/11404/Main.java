703fd76798bc2da81335e3b0b116daca29465142
ksh-code <tjdgnsqn133@gmail.com>
2018-08-01 18:30:36 +0900

    BOJ 백준온라인져지 11404 플로이드 풀이

--- algorithm/BOJ/11404/Main.java
+++ algorithm/BOJ/11404/Main.java
@@ -1,53 +1,44 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 /**
- * BOJ 11404 플로이드
- * https://gist.github.com/KSH-code/59401b2835dbec0dd7c41dea37005269
+ * https://www.acmicpc.net/problem/11404
+ * BOJ 백준온라인져지 11404 플로이드 풀이
  */
-public class Main{
-    private static final int MOD = (int)(1e+9)+7;
-    public static void main(String[] args) throws IOException{
+public class Main {
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-
-        int V = Integer.parseInt(br.readLine());
-        int E = Integer.parseInt(br.readLine());
-        int d[][] = new int[V + 1][V + 1];
-        for(int i = 1; i<=V; i++){
-            Arrays.fill(d[i], Integer.MAX_VALUE);
-            d[i][i] = 0;            
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int edges[][] = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) edges[i][j] = 987654321;
+            edges[i][i] = 0;
         }
-        for (int i = 0; i<E; i++){
-            String str1[] = br.readLine().split(" ");
-            int a = Integer.parseInt(str1[0]);
-            int b = Integer.parseInt(str1[1]);
-            int c = Integer.parseInt(str1[2]);
-            d[a][b] = Math.min(c, d[a][b]);
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            edges[s][e] = Math.min(w, edges[s][e]);
         }
-
-        floyd(d, V);
-
-        for(int i = 1; i<=V; i++){
-            for(int j = 1; j<=V; j++){
-                bw.write(d[i][j] + " ");
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                for (int k = 1; k <= N; k++) {
+                    if (edges[j][k] > edges[j][i] + edges[i][k]) {
+                        edges[j][k] = edges[j][i] + edges[i][k];
+                    }
+                }
+            }
+        }
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                bw.write(String.valueOf(edges[i][j] == 987654321 ? 0 : edges[i][j]));
+                bw.write(' ');
             }
             bw.write("\n");
         }
-
         bw.flush();
-
-    }
-
-    private static void floyd(int d[][], int V){
-        for (int k = 1; k <= V; k++)
-            for (int i = 1; i <= V; i++)
-                for (int j = 1; j <= V; j++) {
-                    if (d[i][k] == Integer.MAX_VALUE || d[k][j] == Integer.MAX_VALUE) continue;
-                    if (d[i][j] > d[i][k] + d[k][j]){
-                        d[i][j] = d[i][k] + d[k][j];
-                    }
-                }
     }
 }
