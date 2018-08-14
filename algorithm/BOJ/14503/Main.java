c056b49f88ad33a967296b13c5cb2cde6229d991
ksh-code <tjdgnsqn133@gmail.com>
2018-08-14 18:17:06 +0900

    BOJ 백준온라인져지 14503 로봇 청소기 풀이

--- algorithm/BOJ/14503/Main.java
+++ algorithm/BOJ/14503/Main.java
@@ -13,40 +13,46 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        int r = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-        int dir = Integer.parseInt(st.nextToken()) - 1;
-        boolean map[][] = new boolean[N][M];
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int dir = Integer.parseInt(st.nextToken());
+        int map[][] = new int[N][M];
         for (int i = 0; i < N; i++) {
             String str = br.readLine();
             for (int j = 0; j < M; j++) {
-                map[i][j] = str.charAt(j * 2) == '0';
+                map[i][j] = str.charAt(j * 2) - (int) '0';
             }
         }
         int result = 1;
-        int xxxx[] = { 0, -1, 0, 1 };
-        int yyyy[] = { -1, 0, 1, 0 };
-        int checkDir = 0;
-        int lastX = -1;
-        int lastY = -1;
+        int xxxx[] = { -1, 0, 1, 0 };
+        int yyyy[] = { 0, 1, 0, -1 };
         while (true) {
-            dir = (dir + 1) % 4;
-            map[r][c] = false;
-            int curX = r + xxxx[dir];
-            int curY = c + yyyy[dir];
-            if (checkDir == 4) dir = (dir + 2) % 4; // 후진했던 경우
-            if (curX >= N || curY >= M || curX < 0 || curY < 0) break;
-            if (map[curX][curY]) {
-                result++;
-                r = curX;
-                c = curY;
-                checkDir = 0;
-                continue;
-            } else {
-                checkDir++;
-                if (checkDir == 5) {
-                    break;
+            map[x][y] = 2;
+            dir = (dir + 3) % 4;
+            int tX = x + xxxx[dir];
+            int tY = y + yyyy[dir];
+            if (tX < N && tY < M && tX >= 0 && tY >= 0) {
+                if (map[tX][tY] == 0) {
+                    result++;
+                    x = tX;
+                    y = tY;
+                    continue;
                 }
+            }
+            int c = 0;
+            for (int i = 0; i < 4; i++) {
+                int ttX = x + xxxx[i];
+                int ttY = y + yyyy[i];
+                if (ttX >= N || ttY >= M || ttX < 0 || ttY < 0 || map[ttX][ttY] > 0) c++;
+            }
+            if (c == 4) {
+                dir = (dir + 3) % 4;
+                int ttX = x + xxxx[dir];
+                int ttY = y + yyyy[dir];
+                if (ttX >= N || ttY >= M || ttX < 0 || ttY < 0 || map[ttX][ttY] == 1) break;
+                x = ttX;
+                y = ttY;
+                dir = (dir + 6) % 4;
             }
         }
         bw.write(String.valueOf(result));
