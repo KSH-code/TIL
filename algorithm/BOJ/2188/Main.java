4622234f365691441b7568ad5227ab2151f0d289
ksh-code <tjdgnsqn133@gmail.com>
2018-09-11 17:29:51 +0900

    BOJ 백준온라인져지 2188 축사 배정 풀이

--- algorithm/BOJ/2188/Main.java
+++ algorithm/BOJ/2188/Main.java
@@ -1,49 +1,45 @@
 import java.util.*;
 import java.io.*;
+
 /**
- * BOJ 2188 축사 배정
- * https://gist.github.com/KSH-code/60dbb61d1af8979e396ca49dc97a3b14
+ * https://www.acmicpc.net/problem/2188
+ * BOJ 백준온라인져지 2188 축사 배정 풀이
  */
-class Main {    
-    private static boolean visited[];   
-    private static int edges[][], N, M, B[];   
-    public static void main(String[] args) throws IOException{
+public class Main {
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static int N, M, room[];
+    private static boolean edges[][], visited[];
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        String str1[] = br.readLine().split(" ");
-        N = Integer.parseInt(str1[0]);
-        M = Integer.parseInt(str1[1]);
-        edges = new int[N+1][M+1];
-        visited = new boolean[N+1];
-        B = new int[N+1];
-        for(int i = 1; i<=N; i++){
-            String str2[] = br.readLine().split(" ");
-            int length = Integer.parseInt(str2[0]);
-            Arrays.fill(edges[i], -1);
-            for(int j = 1; j<=length; j++){
-                edges[i][j] = Integer.parseInt(str2[j]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        edges = new boolean[N + 1][M + 1];
+        room = new int[M + 1];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int K = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < K; j++) {
+                edges[i + 1][Integer.parseInt(st.nextToken())] = true;
             }
         }
-
         int result = 0;
-        for(int i = 1; i<=N; i++){
-            Arrays.fill(visited, false);
-            if(dfs(i)) result++;
+        for (int i = 1; i <= N; i++) {
+            visited = new boolean[N + 1];
+            if (dfs(i)) result++;
         }
-
         bw.write(String.valueOf(result));
         bw.flush();
     }
-    private static boolean dfs(int start){
-        visited[start] = true;
-        for(int i = 1; i<=M; i++){
-            if(edges[start][i] == -1) break;
-            int v = edges[start][i];
-            if(B[v] == 0 || (!visited[B[v]] && dfs(B[v]))){
-                B[v] = start;
-                return true;
-            }
+    private static boolean dfs (int x) {
+        if (visited[x]) return false;
+        visited[x] = true;
+        for (int i = 1; i <= M; i++) {
+            if (edges[x][i])
+                if (room[i] == 0 || dfs(room[i])) {
+                    room[i] = x;
+                    return true;
+                }
         }
         return false;
     }
